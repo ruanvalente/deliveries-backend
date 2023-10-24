02dc4a050b0ba76e2b5ce8da63f3da23127f70cf
ruanvalente <ruanopensuselinux@gmail.com>
2023-10-24 11:40:05 -0300

    feat: update end date deliveryman

---
+++ src/modules/deliveries/use-cases/update-end-date/update-end-date-use-case.ts
@@ -0,0 +1,23 @@
+import { prisma } from "../../../../database/prisma-client";
+import { IUseCases } from "../../../../shared/implements/use-cases/use-cases";
+
+interface IUpdateEndDase {
+  id_delivery: string;
+  id_deliveryman: string;
+}
+
+export class UpdateEndDateUseCase implements IUseCases {
+  async execute({ id_delivery, id_deliveryman }: IUpdateEndDase) {
+    const delivery = await prisma.deliveries.updateMany({
+      where: {
+        id: id_delivery,
+        id_deliveryman,
+      },
+      data: {
+        end_at: new Date(),
+      },
+    });
+
+    return delivery;
+  }
+}
